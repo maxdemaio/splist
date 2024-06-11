2fd8de242729d590b8f3e11b889ed9d70e69a83e
maxdemaio <maxdman321@gmail.com>
2024-06-11 11:34:30 -0400

    navbar and icons

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -12,29 +12,31 @@ export default function Navbar(props: INavbarProps) {
   console.log(props);
   return (
     <div className="flex flex-col gap-8">
-      <nav className="text-lg grid grid-cols-2 lg:grid-cols-3 md:p-0">
+      <nav className="text-lg flex justify-between md:p-0">
         <div className="flex items-center text-3xl md:text-4xl">Splist~</div>
-        {props.userName && (
-          <div className="hidden lg:flex items-center justify-center gap-4">
-            {props.userImage ? (
-              <img
-                src={props.userImage}
-                alt="User image"
-                className="rounded-full"
-              />
-            ) : (
-              <Icons.profileDefault />
-            )}
-            <span>Signed in as {props.userName}</span>
-          </div>
-        )}
-
         {props.showSignOut && (
           <div className="flex items-center justify-end md:gap-12 gap-4">
             <div className="flex gap-2 items-center">
               <Icons.github />
               <span className="hidden md:flex">GitHub</span>
             </div>
+            {props.userName && (
+              <div className="hidden lg:flex items-center justify-center gap-4">
+                {props.userImage ? (
+                  <img
+                    src={props.userImage}
+                    alt="User image"
+                    className="rounded-full"
+                    width={50}
+                    height={50}
+                  />
+                ) : (
+                  <Icons.profileDefault />
+                )}
+                <span>Signed in as {props.userName}</span>
+              </div>
+            )}
+
             <Button
               variant={"secondary"}
               className="text-lg"
@@ -51,6 +53,8 @@ export default function Navbar(props: INavbarProps) {
             src={props.userImage}
             alt="User image"
             className="rounded-full"
+            width={50}
+            height={50}
           />
         ) : (
           <Icons.profileDefault />
