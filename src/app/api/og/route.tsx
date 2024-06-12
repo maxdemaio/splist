b3993654aa85c6a1a190c2d97875e58bd42314b2
maxdemaio <maxdman321@gmail.com>
2024-06-12 11:35:55 -0400

    update prod link

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -11,7 +11,7 @@ export async function POST(request: Request) {
   const year = date.getFullYear();
 
   const HOST =
-    process.env.NODE_ENV === "production" ? "https://splist.vercel.app" : "http://localhost:3000";
+    process.env.NODE_ENV === "production" ? "https://splist-lac.vercel.app" : "http://localhost:3000";
 
   const body = await request.json();
   const { topArtists, topTracks } = body;
