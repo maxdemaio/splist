f302721279c84299178359a6e478fe41ce4c5630
maxdemaio <maxdman321@gmail.com>
2024-06-15 19:05:19 -0400

    resize default img

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -19,21 +19,21 @@ export const Icons = {
   profileDefault: function ProfileDefaultIcon(props: any) {
     return (
       <svg
-        width="64"
-        height="64"
-        viewBox="0 0 64 64"
+        width="50"
+        height="50"
+        viewBox="0 0 50 50"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
         <g clipPath="url(#clip0_23_90)">
           <path
-            d="M32 64C49.6731 64 64 49.6731 64 32C64 14.3269 49.6731 0 32 0C14.3269 0 0 14.3269 0 32C0 49.6731 14.3269 64 32 64Z"
-            fill="#5272A1"
+            d="M25 50C38.8071 50 50 38.8071 50 25C50 11.1929 38.8071 0 25 0C11.1929 0 0 11.1929 0 25C0 38.8071 11.1929 50 25 50Z"
+            fill="#528EA1"
           />
         </g>
         <defs>
           <clipPath id="clip0_23_90">
-            <rect width="64" height="64" fill="white" />
+            <rect width="50" height="50" fill="white" />
           </clipPath>
         </defs>
       </svg>
