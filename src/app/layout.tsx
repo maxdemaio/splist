91c09776ccf34b77d57bd415c7a97d74ff893abb
maxdemaio <maxdman321@gmail.com>
2024-06-15 23:35:38 -0400

    company secrets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,26 +3,27 @@ import "./globals.css";
 import AuthSessionProvider from "@/components/AuthSessionProvider";
 import { getServerSession } from "next-auth";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   openGraph: {
-    title: 'Splist',
-    description: 'Share your top artists and songs with friends!',
-    url: 'https://splist-lac.vercel.app/',
-    siteName: 'Splist',
+    title: "Splist",
+    description: "Share your top artists and songs with friends!",
+    url: "https://splist-lac.vercel.app/",
+    siteName: "Splist",
     images: [
       {
-        url: 'https://splist-lac.vercel.app/og.png', // Must be an absolute URL
+        url: "https://splist-lac.vercel.app/og.png", // Must be an absolute URL
         width: 1200,
         height: 630,
       },
     ],
-    locale: 'en_US',
-    type: 'website',
+    locale: "en_US",
+    type: "website",
   },
-}
+};
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession(authOptions);
@@ -30,7 +31,10 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html className="dark" lang="en">
       <AuthSessionProvider session={session}>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          {children}
+          <Analytics mode={"production"} />;
+        </body>
       </AuthSessionProvider>
     </html>
   );
