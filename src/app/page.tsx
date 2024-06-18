66402cd6e0d1f8a8920188c2a6374de10162ce89
maxdemaio <maxdman321@gmail.com>
2024-06-17 21:15:10 -0400

    hodl?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,10 +134,10 @@ function SpotifySearch({ sdk, toast }: { sdk: SpotifyApi; toast: any }) {
         const blob = await res.blob();
 
         // Check for navigator apis
-        if (typeof navigator?.clipboard?.write === "function") {
-          await copyImageToClipboard(blob);
-        } else if (typeof navigator?.share === "function") {
+        if (typeof navigator?.share === "function" && typeof navigator?.canShare === "function") {
           await shareImage(blob);
+        } else if (typeof navigator?.clipboard?.write === "function") {
+          await copyImageToClipboard(blob);
         } else {
           handleUnsupportedAPI();
         }
