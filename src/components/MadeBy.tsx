e6516df58fdb662f62a41dad95576003c7e0655b
maxdemaio <maxdman321@gmail.com>
2024-06-17 23:09:24 -0400

    listen to hackysore

--- src/components/MadeBy.tsx
+++ src/components/MadeBy.tsx
@@ -1,27 +1,28 @@
+import { Icons } from "./icons";
+
 export default function MadeBy() {
   return (
-    <div className="items-center gap-4 w-full flex flex-col justify-center">
+    <div className="opacity-80 items-center gap-4 w-full flex flex-col justify-center">
       <div>
-        <span>
-          Made with ❤️ by{" "}
-          <a
-            className="underline transition-all duration-150 hover:opacity-80"
-            href="https://github.com/maxdemaio"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Max DeMaio
-          </a>
-        </span>
+        <a
+          className="border-b-[1px] border-off border-neutral-700 group opacity-80 flex items-center gap-[0.45rem] transition-all duration-300 hover:opacity-60"
+          href="https://github.com/sponsors/maxdemaio"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span>Made by Max DeMaio</span>
+          <Icons.heart />
+        </a>
       </div>
 
       <a
         href="https://github.com/maxdemaio/splist"
-        className="underline transition-all duration-150 hover:opacity-80"
+        className="border-b-[1px] border-off border-neutral-700 group opacity-80 flex items-center gap-[0.45rem] transition-all duration-300 hover:opacity-60"
         target="_blank"
         rel="noopener noreferrer"
       >
-        💫 Star on GitHub
+        <span>Star on GitHub</span>
+        <Icons.github />
       </a>
     </div>
   );
